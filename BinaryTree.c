4bbff01919fdb9abb83b715e3e59fdffc41258d5
SeokWonLee <sevenshards@hanyang.ac.kr>
2023-11-23 20:03:00 +0900

    이진 트리 구현 (전위, 중위, 후위 순회 및 트리 삭제 구현)

--- BinaryTree.c
+++ BinaryTree.c
@@ -1,9 +1,12 @@
 /*
-* 자료구조 - 이진트리 구현
-* 작성자: Sevenshards
-* 작성 일자: 2023.11.23
 * 비선형 자료구조 - 연결 리스트 기반 이진 트리
 * 파일명: BinaryTree.c
+* 파일 버전: 0.2
+* 작성자: Sevenshards
+* 작성 일자: 2023-11-23
+* 이전 버전 작성 일자: 2023-11-23
+* 버전 내용: 전위, 중위 순회 추가 및 순회 시 할 일을 결정할 수 있도록 함수 포인터 사용.
+* 이전 버전 내용: 중위 순회 기능까지 포함한 이진 트리 구현.
 */
 // 저자의 표현을 빌리자면, 이진 트리를 구현"할 수 있는 도구"를 만드는 과정이다.
 // 어디다 갖다 쓸 것인가는 나중에 생각하자.
@@ -20,12 +23,12 @@ BTreeNode* MakeBTreeNode(void) // 이진 트리의 노드 생성
 	return node;
 }
 
-BTData GetData(BTreeNode* bt) // 이진 트리 노드의 데이터를 반환
+BTData GetData(BTreeNode* bt) // 이진 트리 노드의 데이터를 반환 (Getter)
 {
 	return bt->data;
 }
 
-void SetData(BTreeNode* bt, BTData data) // 이진 트리 노드에 데이터를 저장
+void SetData(BTreeNode* bt, BTData data) // 이진 트리 노드에 데이터를 저장 (Setter)
 {
 	bt->data = data;
 }
@@ -54,4 +57,50 @@ void MakeRightSubTree(BTreeNode* main, BTreeNode* sub) // 매개 변수 sub로 전달된
 		free(main->right); // 할당된 메모리를 해제한다.
 
 	main->right = sub; // 그리고 새로 오른쪽에 연결한다.
+}
+
+// 순회 (재귀를 이용하여 구현한다!)
+// 중위 순회 1)왼쪽 서브 트리 -> 2)루트 노드 -> 3)오른쪽 서브 트리
+void InorderTraversal(BTreeNode* bt, VisitFuncPtr action)
+{
+	if (bt == NULL) // 만약 공집합 노드(NULL)이라면? 재귀의 탈출 조건이 된다.
+		return;
+
+	InorderTraversal(bt->left, action); // 1) 왼쪽 서브 트리를 순회
+	action(bt->data); // 2) 루트 노드를 방문
+	InorderTraversal(bt->right, action); // 3) 오른쪽 서브 트리를 순회
+}
+// 전위 순회 1)루트 노드 -> 2)왼쪽 서브 트리 -> 3)오른쪽 서브 트리
+void PreorderTraversal(BTreeNode* bt, VisitFuncPtr action)
+{
+	if (bt == NULL) // 만약 공집합 노드(NULL)이라면? 재귀의 탈출 조건이 된다.
+		return;
+
+	action(bt->data); // 1) 루트 노드를 방문
+	PreorderTraversal(bt->left, action); // 2) 왼쪽 서브 트리를 순회
+	PreorderTraversal(bt->right, action); // 3) 오른쪽 서브 트리를 순회
+}
+// 후위 순회 1)왼쪽 서브 트리 -> 2)오른쪽 서브트리 -> 3)루트 노드
+void PostorderTraversal(BTreeNode* bt, VisitFuncPtr action)
+{
+	if (bt == NULL) // 만약 공집합 노드(NULL)이라면? 재귀의 탈출 조건이 된다.
+		return;
+
+	PostorderTraversal(bt->left, action); // 1) 왼쪽 서브 트리를 순회
+	PostorderTraversal(bt->right, action); // 2) 오른쪽 서브 트리를 순회
+	action(bt->data); // 3) 루트 노드를 방문
+}
+
+// 트리의 모든 노드 삭제
+void DeleteTree(BTreeNode* bt)
+{
+	if (bt == NULL) // 공집합 노드(NULL)이라면 재귀 탈출.
+		return;
+
+	// 후위 순회를 통해 왼쪽, 그리고 오른쪽, 마지막으로 루트 노드를 비우는 형태로 진행.
+	DeleteTree(bt->left);
+	DeleteTree(bt->right);
+
+	printf("delete tree node data: %d\n", bt->data);
+	free(bt);
 }
--- BinaryTreeMain.c
+++ BinaryTreeMain.c
@@ -1,25 +1,18 @@
 /*
-* 자료구조 - 이진트리 구현
-* 작성자: Sevenshards
-* 작성 일자: 2023.11.23
 * 비선형 자료구조 - 연결 리스트 기반 이진 트리
-* 파일명: BinaryTree.c
+* 파일명: BinaryTreeMain.c
+* 파일 버전: 0.2
+* 작성자: Sevenshards
+* 작성 일자: 2023-11-23
+* 이전 버전 작성 일자: 2023-11-23
+* 버전 내용: 전위, 중위 순회 추가 및 순회 시 할 일을 결정할 수 있도록 함수 포인터 사용.
+* 이전 버전 내용: 중위 순회 기능까지 포함한 이진 트리 구현.
 */
 
 #include <stdio.h>
 #include "BinaryTree.h"
 
-// 순회 (재귀를 이용하여 구현한다!)
-// 중위 순회 1) 왼쪽 서브 트리 2) 루트 노드 3) 오른쪽 서브 트리
-void InorderTraversal(BTreeNode* bt)
-{
-	if (bt == NULL) // 만약 공집합 노드(NULL)이라면? 재귀의 탈출 조건이 된다.
-		return;
-
-	InorderTraversal(bt->left); // 1) 왼쪽 서브 트리를 순회
-	printf("%d\n", bt->data); // 2) 루트 노드를 방문
-	InorderTraversal(bt->right); // 3) 오른쪽 서브 트리를 순회
-}
+void ShowIntData(int data);
 
 int main()
 {
@@ -28,22 +21,36 @@ int main()
 	BTreeNode* bt2 = MakeBTreeNode();
 	BTreeNode* bt3 = MakeBTreeNode();
 	BTreeNode* bt4 = MakeBTreeNode();
+	BTreeNode* bt5 = MakeBTreeNode();
+	BTreeNode* bt6 = MakeBTreeNode();
 
 	// 노드에 데이터 저장
 	SetData(bt1, 1);
 	SetData(bt2, 2);
 	SetData(bt3, 3);
 	SetData(bt4, 4);
+	SetData(bt5, 5);
+	SetData(bt6, 6);
 
 	MakeLeftSubTree(bt1, bt2); // bt1의 왼쪽 자식 노드로 bt2 연결
 	MakeRightSubTree(bt1, bt3); // bt1의 오른쪽 자식 노드로 bt3 연결
 	MakeLeftSubTree(bt2, bt4); // bt2의 왼쪽 자식 노드로 bt4 연결
+	MakeRightSubTree(bt2, bt5); // bt2의 오른쪽 자식 노드로 bt5 연결
+	MakeRightSubTree(bt3, bt6); // bt3의 오른쪽 자식 노드로 bt6 연결
 
-	//printf("%d\n", GetData(GetLeftSubTree(bt1))); // bt1의 왼쪽 자식 노드 데이터 출력
-	//printf("%d\n", GetData(GetRightSubTree(bt1))); // bt1의 오른쪽 자식 노드 데이터 출력
-	//printf("%d\n", GetData(GetLeftSubTree(GetLeftSubTree(bt1)))); // bt1의 왼쪽 자식 노드의 왼쪽 자식 노드의 데이터 출력
+	PreorderTraversal(bt1, ShowIntData);
+	printf("\n");
+	InorderTraversal(bt1, ShowIntData);
+	printf("\n");
+	PostorderTraversal(bt1, ShowIntData);
+	printf("\n");
 
-	InorderTraversal(bt1);
+	DeleteTree(bt1);
 
 	return 0;
+}
+
+void ShowIntData(int data)
+{
+	printf("%d ", data);
 }
