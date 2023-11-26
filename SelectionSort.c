db252f8e084cde331a3d92282c396f7c5c340448
SeokWonLee <sevenshards@hanyang.ac.kr>
2023-11-26 17:29:25 +0900

    주석 추가

--- SelectionSort.c
+++ SelectionSort.c
@@ -14,18 +14,19 @@
 void SelectionSort(int arr[], int n)
 {
 	int i, j;
-	int maxIdx;
+	int maxIdx; // 우선 순위가 가장 높은 인덱스 값
 	int temp;
 
 	for (i = 0; i < n - 1; i++)
 	{
-		maxIdx = i;
+		maxIdx = i; // 인덱스 값은 i로. 맨 처음이라면 0부터
 		for (j = i + 1; j < n; j++)
 		{
-			if (arr[j] < arr[maxIdx])
-				maxIdx = j;
+			if (arr[j] < arr[maxIdx]) // 오름차순 기준 정렬
+				maxIdx = j; // 만약 비교하는 값이 더 작다면 해당 인덱스로 바꾼다.
 		}
 
+		// 데이터 교환
 		temp = arr[i];
 		arr[i] = arr[maxIdx];
 		arr[maxIdx] = temp;
