04fbc17211511b0077a2b4fe052107744148f191
SeokWonLee <sevenshards@hanyang.ac.kr>
2023-11-26 18:05:22 +0900

    주석 변경

--- InsertionSort.c
+++ InsertionSort.c
@@ -16,14 +16,14 @@ void InsertionSort(int arr[], int n)
 	int i, j;
 	int insertData;
 
-	for (i = 1; i < n; i++) // 첫 번째가 아닌 두 번째 요소부터 마지막 요소까지
+	for (i = 1; i < n; i++) // 첫 번째가 아닌 두 번째 요소부터 마지막 요소까지 -> 첫 번째 요소는 데이터가 정렬이 되었다고 보기 때문
 	{
 		insertData = arr[i]; // 정렬 대상을 별도로 저장
 
 		for (j = i - 1; j >= 0; j--) // i-1 번째 요소부터 첫 번째 요소까지 역순으로
 		{
 			if (arr[j] > insertData)
-				arr[j + 1] = arr[j]; // 비교 대상을 한 칸 앞으로 이동
+				arr[j + 1] = arr[j]; // 비교 대상을 한 칸 이동
 			else
 				break; // 삽입 위치를 찾았으므로 반복문 탈출
 		}
