c7febf1a78a646d4504f0c858238c80b3fb6af78
SeokWonLee <sevenshards@hanyang.ac.kr>
2023-11-27 16:29:11 +0900

    이해를 돕기 위한 주석 및 변수명 변경

--- RadixSort.c
+++ RadixSort.c
@@ -20,8 +20,8 @@ void RadixSort(int arr[], int num, int maxLen)
 	Queue buckets[BUCKET_NUM];
 	int bucket_idx; // 버킷에 사용할 인덱스
 	int pos;
-	int digit_idx; // 자릿수에 사용할 인덱스
-	int divfac = 1; // 자릿수에 사용할 변수
+	int data_idx; // 데이터에 사용할 인덱스
+	int div_factor = 1; // 자릿수를 구하는데 사용하는 피제수
 	int radix;
 
 	// 10개의 버킷 초기화
@@ -31,25 +31,27 @@ void RadixSort(int arr[], int num, int maxLen)
 	// 가장 긴 데이터의 길이만큼 반복
 	for (pos = 0; pos < maxLen; pos++)
 	{
-		for (digit_idx = 0; digit_idx < num; digit_idx++)
+		// 버킷에 데이터를 넣는 부분
+		for (data_idx = 0; data_idx < num; data_idx++)
 		{
 			// N번째 자리의 숫자 추출
-			radix = (arr[digit_idx] / divfac) % 10;
+			radix = (arr[data_idx] / div_factor) % 10;
 
 			// 추출한 숫자를 근거로 버킷에 데이터를 저장
-			QEnqueue(&buckets[radix], arr[digit_idx]);
+			QEnqueue(&buckets[radix], arr[data_idx]);
 		}
 
+		// 버킷에서 데이터를 추출하는 부분
 		// 버킷의 수 만큼 반복
-		for (bucket_idx = 0, digit_idx = 0; bucket_idx < BUCKET_NUM; bucket_idx++)
+		for (bucket_idx = 0, data_idx = 0; bucket_idx < BUCKET_NUM; bucket_idx++)
 		{
 			// 버킷에 저장된 것을 순서대로 다 꺼내서 다시 arr에 저장
 			while (!QisEmpty(&buckets[bucket_idx]))
-				arr[digit_idx++] = QDequeue(&buckets[bucket_idx]);
+				arr[data_idx++] = QDequeue(&buckets[bucket_idx]);
 		}
 
 		// N번째 자리의 숫자 추출을 위해서 피제수의 증가
-		divfac *= 10;
+		div_factor *= 10;
 	}
 }
 
