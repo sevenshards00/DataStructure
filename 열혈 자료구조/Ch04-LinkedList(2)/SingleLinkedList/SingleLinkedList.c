cb838c73342e8405934de6a7b193e3de3d6f1bda
Sevenshards <sevenshards@hanyang.ac.kr>
2023-11-30 15:25:48 +0900

    정렬 기준 삽입 추가 및 로직 수정

--- 열혈 자료구조/Ch04-LinkedList(2)/SingleLinkedList/SingleLinkedList.c
+++ 열혈 자료구조/Ch04-LinkedList(2)/SingleLinkedList/SingleLinkedList.c
@@ -1,6 +1,13 @@
-// 11.02 SingleLinkedList 복기 2회차
-// SingleLinkedList.c
-//
+/*
+* 선형 자료구조 - 단일 연결 리스트 (더미 노드 기반)
+* 파일명: SingleLinkedList.c
+* 파일 버전: 0.3
+* 작성자: Sevenshards
+* 작성 일자: 2023-11-30
+* 이전 버전 작성 일자: 2023-11-29
+* 버전 내용: 정렬 기준 삽입 추가 및 로직 수정
+* 이전 버전 내용: 해시 충돌 문제 해결을 위한 체이닝 기법 구현
+*/
 
 #include <stdlib.h>
 #include "SingleLinkedList.h"
@@ -13,17 +20,42 @@ void ListInit(List* plist)
 	plist->numOfData = 0; // 그리고 더미노드를 제외하고 실제 데이터 수를 기록해야하므로 0이다. -1이 아니다.
 }
 
+void FInsert(List* plist, LData data) // 정렬 기준 없이 리스트의 노드를 생성할 때 사용
+{
+	Node* newNode = (Node*)malloc(sizeof(Node)); // 새로운 노드 메모리 할당
+	newNode->data = data; // 노드에 데이터 입력
+	newNode->next = plist->head->next; // head부터 데이터가 들어가기 때문에, 더미노드 다음을 가리키고 있는 것을 새로운 노드가 가리키게 해야함
+	plist->head->next = newNode; // head에서부터 들어가므로 더미노드 다음을 새로운 노드를 가리키게 함
+	(plist->numOfData)++; // 데이터 수 증가
+}
+
+// 정렬 기준에 맞춰서 리스트의 노드를 생성할 때 사용 (Sort Insert)
+void SInsert(List* plist, LData data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node)); // 새로운 노드 메모리 할당
+	newNode->data = data; // 노드에 데이터 입력
+	Node* cur = plist->head; // 삽입을 위해 사용하는 현재 위치(current), 시작 위치는 더미노드
+	
+	// 새 노드가 들어갈 위치를 찾기 위한 반복문 수행
+	// 더 이상 읽을 노드가 없을 때까지, 그리고 새 노드에 들어갈 데이터와 기존 노드의 데이터를 비교하여 기준에 맞을 때까지 반복
+	while (cur->next != NULL && plist->comp(data, cur->next->data) != 0)
+	{
+		cur = cur->next; // 정렬 기준에 맞는 위치까지 다음 노드로 이동
+	}
+
+	// 반복문이 끝났다면 노드를 삽입할 위치(cursor)를 찾은 것
+	newNode->next = cur->next; // cur의 다음 위치에 새로운 노드가 들어가기 때문에, cur의 다음을 새로운 노드가 가리키도록 해야함
+	cur->next = newNode; // 그리고 cur의 next를 새로운 노드를 가리키게 함
+	(plist->numOfData)++; // 데이터 수 증가
+}
+
 // 리스트의 노드를 새로 생성할 때 사용
 void LInsert(List* plist, LData data)
 {
-	Node* newNode = malloc(sizeof(Node)); // 새로운 노드 메모리 할당
-	newNode->data = data; // 노드에 데이터 입력
-
-	// 잘못 쓴 부분. 여기를 잘못 써서 다 꼬였다.
-	// newNode->next = NULL; // 새로운 노드가 가리키는 다음 노드는 없으므로 NULL
-	newNode->next = plist->head->next; // head부터 데이터가 들어가기 때문에, 항상 더미노드 다음을 가리키고 있는 것을 새로운 노드가 가리키게 해야함
-	plist->head->next = newNode; // head에서부터 들어가므로 더미노드 다음을 새로운 노드를 가리키게 함
-	(plist->numOfData)++; // 데이터 수 증가
+	if (plist->comp == NULL) // 정렬 기준이 없다면
+		FInsert(plist, data); // FInsert 호출
+	else // 정렬 기준이 있다면
+		SInsert(plist, data); // SInsert 호출
 }
 
 // 리스트의 맨 첫번째 데이터를 참조할 때 사용
@@ -51,9 +83,10 @@ int LNext(List* plist, LData* pdata)
 	if (plist->cur->next == NULL) // 현재 위치 다음이 가리키는 노드가 없다면
 		return FALSE; // 데이터가 없다
 
-	*pdata = plist->cur->data; // 현재 가리키는 위치의 데이터 참조
+	
 	plist->before = plist->cur; // before에 현재 가리키는 노드 위치를 저장
 	plist->cur = plist->cur->next; // 현재 가리키는 위치를 하나 앞으로 이동
+	*pdata = plist->cur->data; // 현재 가리키는 위치의 데이터 참조 -> 데이터 참조 위치를 잘못 지정하였음
 
 	return TRUE; // 데이터 참조 성공
 }
@@ -79,4 +112,9 @@ LData LRemove(List* plist)
 int LCount(List* plist)
 {
 	return plist->numOfData;
+}
+// 리스트에 정렬 기준 함수를 등록
+void SetSortRule(List* plist, CompareFunc pcomp)
+{
+	plist->comp = pcomp;
 }
--- 열혈 자료구조/Ch04-LinkedList(2)/SingleLinkedList/SingleLinkedListMain.c
+++ 열혈 자료구조/Ch04-LinkedList(2)/SingleLinkedList/SingleLinkedListMain.c
@@ -1,10 +1,27 @@
-// 11.02 SingleLinkedList 복기 2회차
-// SingleLinkedListMain.c
-//
+/*
+* 선형 자료구조 - 단일 연결 리스트 (더미 노드 기반)
+* 파일명: SingleLinkedListMain.c
+* 파일 버전: 0.3
+* 작성자: Sevenshards
+* 작성 일자: 2023-11-30
+* 이전 버전 작성 일자: 2023-11-29
+* 버전 내용: 정렬 기준 삽입 추가
+* 이전 버전 내용: 해시 충돌 문제 해결을 위한 체이닝 기법 구현
+*/
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include "SingleLinkedList.h"
+
+// 우선순위 비교를 위한 함수 정의
+int WhoIsPrecede(int d1, int d2)
+{
+	// 오름차순 기준
+	if (d1 < d2)
+		return 0; // d1이 정렬 순서상 앞선다
+	else
+		return 1; // d2가 정렬 순서상 앞서거나 같다
+}
 
 int main()
 {
@@ -13,12 +30,14 @@ int main()
 	int data;
 	ListInit(&list);
 
+	SetSortRule(&list, WhoIsPrecede); // 정렬의 기준 등록
+
 	// 데이터 5개 입력
 	LInsert(&list, 11);
+	LInsert(&list, 11);
+	LInsert(&list, 22);
 	LInsert(&list, 22);
 	LInsert(&list, 33);
-	LInsert(&list, 44);
-	LInsert(&list, 55);
 
 	// 저장된 데이터 개수 출력 및 전체 데이터 출력
 	printf("Stored Data Count: %d\n", LCount(&list));
