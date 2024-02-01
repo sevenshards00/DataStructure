d66ae2fe72979ca0337ff8010de34f45f62527e7
Sevenshards <sevenshards@hanyang.ac.kr>
2024-02-01 10:32:29 +0900

    STL의 forward_list 흉내내기 (프로토타입 완성)

--- 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedList.h
+++ 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedList.h
@@ -1,12 +1,12 @@
 ﻿/*
 * 이것이 자료구조+알고리즘이다 - 리스트(List) - 단일 연결 리스트
 * 파일명: SingleLinkedList.h
-* 파일 버전: 0.2
+* 파일 버전: 0.3
 * 작성자: Sevenshards
-* 작성 일자: 2024-01-31
-* 이전 버전 작성 일자: 2024-01-29
-* 버전 내용: STL의 forward_list 흉내내기 - 클래스명 변경 및 내부 구현 변경 (미완)
-* 이전 버전 내용: 단일 연결 리스트 구현
+* 작성 일자: 2024-02-01
+* 이전 버전 작성 일자: 2024-01-31
+* 버전 내용: STL의 forward_list 흉내내기 (프로토타입 완성)
+* 이전 버전 내용: STL의 forward_list 흉내내기 - 클래스명 변경 및 내부 구현 변경 (미완)
 */
 
 #pragma once
@@ -40,11 +40,11 @@ public:
 	// 노드 삽입(insert_after) - 특정 위치 다음에 삽입하는 경우
 	bool insert_after(Node<T>* curr, CONST T &newData);
 	// 리스트의 처음 노드 위치를 반환 (begin)
-	Node *begin();
+	Node<T> *begin();
 	// 리스트의 끝 다음 노드 위치를 반환 (end)
-	Node *end();
+	Node<T> *end();
 	// 노드 삭제(pop_front) - head에서 노드 제거
-	bool pop_front();
+	bool pop_front(T *nodeData);
 	// 노드 삭제(erase_after) - 특정 위치 다음 노드 제거
 	bool erase_after(Node<T> *curr, T* nodeData);
 	// 노드 갯수(Count)
@@ -69,7 +69,7 @@ inline ForwardList<T>::~ForwardList()
 	Node<T> *curr = head->next;
 	Node<T> *before = head;
 
-	while (curr->next == tail)
+	while (curr->next != tail)
 	{
 		delete before;
 		before = curr;
@@ -92,68 +92,69 @@ inline void ForwardList<T>::push_front(CONST T &newData)
 }
 // // 노드 삽입(insert_after) - 특정 위치 다음에 삽입하는 경우
 template <typename T>
-inline bool ForwardList<T>::insert_after(Node *curr, CONST T &newData)
+inline bool ForwardList<T>::insert_after(Node<T> *curr, CONST T &newData)
 {
 	Node<T> *newNode = new Node<T>();
 	newNode->data = newData;
 
-	DWORD insertIdx = 0;
-	Node<T> *curr = head->next;
-	Node<T> *before = head;
-	
-
-	if (idx >= nodeCnt)
-		return false;
-
-	while (idx != insertIdx)
-	{
-		before = curr;
-		curr = curr->next;
-		insertIdx++;
-	}
-
 	newNode->next = curr->next;
 	curr->next = newNode;
-	nodeCnt++;
+	++nodeCnt;
 
 	return true;
 }
 
+// 리스트의 처음 노드 위치를 반환 (begin)
 template <typename T>
-inline Node *ForwardList<T>::begin()
+inline Node<T> *ForwardList<T>::begin()
 {
-
+	return head->next;
 }
 
+// 리스트의 끝 다음 노드 위치를 반환 (end)
 template <typename T>
-inline Node *ForwardList<T>::end()
+inline Node<T> *ForwardList<T>::end()
 {
-
-}
-
-// 노드 삭제(Destroy) - 대상 노드 제거
-template <typename T>
-inline bool ForwardList<T>::DestroyNode(DWORD idx, T *nodeData)
-{
-	DWORD removeIdx = 0;
 	Node<T> *curr = head->next;
-	Node<T> *before = head;
-	
-	if (idx >= nodeCnt)
+	while (curr->next != tail)
+		curr = curr->next;
+
+	return curr;
+}
+
+// 노드 삭제(pop_front) - head에서 노드 제거
+template <typename T>
+inline bool ForwardList<T>::pop_front(T *nodeData)
+{
+	Node<T> *dNode = head->next;
+	if (dNode == tail)
 		return false;
 
-	while (removeIdx != idx)
-	{
-		before = curr;
-		curr = curr->next;
-		removeIdx++;
-	}
+	head->next = dNode->next;
+	*nodeData = dNode->data;
+	delete dNode;
+	--nodeCnt;
 
-	*nodeData = curr->data;
-	before->next = curr->next;
-	nodeCnt--;
-	delete curr;
+	return true;
+
 }
+// 노드 삭제(erase_after) - 특정 위치 다음 노드 제거
+template <typename T>
+inline bool ForwardList<T>::erase_after(Node<T> *curr, T *nodeData)
+{
+	// 노드가 없거나 하나만 있을 경우에는 특정 위치 다음에 지울 것이 없으므로 삭제하지 않는다.
+	if (curr->next == tail || head->next == tail)
+		return false;
+
+	Node<T> *dNode = curr->next;
+	curr->next = dNode->next;
+	*nodeData = dNode->data;
+	delete dNode;
+	--nodeCnt;
+
+	return true;
+}
+
 // 노드 갯수(Count)
 template <typename T>
 inline DWORD ForwardList<T>::Count() const
--- 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedListMain.cpp
+++ 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedListMain.cpp
@@ -1,55 +1,59 @@
 ﻿/*
 * 이것이 자료구조+알고리즘이다 - 리스트(List) - 단일 연결 리스트
-* 파일명: SingleLinkedList.h
-* 파일 버전: 0.2
+* 파일명: SingleLinkedListMain.cpp
+* 파일 버전: 0.3
 * 작성자: Sevenshards
-* 작성 일자: 2024-01-31
-* 이전 버전 작성 일자: 2024-01-29
-* 버전 내용: STL의 forward_list 흉내내기 - 클래스명 변경 및 내부 구현 변경 (미완)
-* 이전 버전 내용: 단일 연결 리스트 구현
+* 작성 일자: 2024-02-01
+* 이전 버전 작성 일자: 2024-01-31
+* 버전 내용: STL의 forward_list 흉내내기 (프로토타입 완성)
+* 이전 버전 내용: STL의 forward_list 흉내내기 - 클래스명 변경 및 내부 구현 변경 (미완)
 */
 
 #include "SingleLinkedList.h"
 
 int _tmain(int argc, TCHAR *argv[])
 {
-	SingleLinkedList<INT> listInt;
+	ForwardList<INT> intList;
 
-	listInt.AppendFront(3);
-	listInt.AppendFront(2);
-	listInt.AppendFront(1);
-	listInt.AppendBack(5);
-	listInt.AppendBack(6);
-	listInt.InsertNode(2, 4);
+	intList.push_front(1);
+	intList.push_front(2);
+	intList.push_front(5);
+	intList.insert_after(intList.begin(), 3);
+	intList.insert_after(intList.begin(), 4);
 
 	DWORD i = 0;
-	INT nodeData;
-	while (true)
+	Node<INT> *intList_begin = intList.begin();
+	Node<INT> *intList_end = intList.end();
+
+	_tprintf(TEXT("begin: %d\n"), intList_begin->data);
+	_tprintf(TEXT("end: %d\n"), intList_end->data);
+
+	for(i = 0; i != intList.Count(); ++i)
 	{
-		listInt.SearchNode(i++, &nodeData);
-		_tprintf(TEXT("%d "), nodeData);
-		
-		if (i == listInt.Count())
-			break;
+		_tprintf(TEXT("%d "), intList_begin -> data);
+		intList_begin = intList_begin->next;
 	}
 
 	_tprintf(TEXT("\n"));
 
-	listInt.DestroyNode(2, &nodeData);
+	INT nodeData;
+
+	intList.pop_front(&nodeData);
+	_tprintf(TEXT("Remove Data: %d\n"), nodeData);
+	intList.erase_after(intList.begin(), &nodeData);
 	_tprintf(TEXT("Remove Data: %d\n"), nodeData);
 
 
-	i = 0;
-	while (true)
-	{
-		listInt.SearchNode(i++, &nodeData);
-		_tprintf(TEXT("%d "), nodeData);
+	intList_begin = intList.begin();
 
-		if (i == listInt.Count())
-			break;
+	for (i = 0; i != intList.Count(); ++i)
+	{
+		_tprintf(TEXT("%d "), intList_begin->data);
+		intList_begin = intList_begin->next;
 	}
 
-	listInt.Count();
+	_tprintf(TEXT("\n"));
+	_tprintf(TEXT("Node Count: %d\n"), intList.Count());
 
 	return 0;
 }
