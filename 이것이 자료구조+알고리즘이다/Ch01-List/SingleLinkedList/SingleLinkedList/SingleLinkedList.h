3c3fbe1b58762d1cf3a42e75628ebbb58fda85d8
Sevenshards <sevenshards@hanyang.ac.kr>
2024-02-01 21:31:31 +0900

    단일 연결 리스트 다시 구현하기(미완)

--- 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedList.h
+++ 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedList.h
@@ -1,15 +1,21 @@
 ﻿/*
 * 이것이 자료구조+알고리즘이다 - 리스트(List) - 단일 연결 리스트
 * 파일명: SingleLinkedList.h
-* 파일 버전: 0.4
+* 파일 버전: 0.5
 * 작성자: Sevenshards
 * 작성 일자: 2024-02-01
-* 이전 버전 작성 일자: 2024-01-31
-* 버전 내용: STL의 forward_list 흉내내기 (소멸자 로직 수정)
-* 이전 버전 내용: STL의 forward_list 흉내내기 (프로토타입 완성)
+* 이전 버전 작성 일자: 2024-02-01
+* 버전 내용: 단일 연결 리스트 다시 구현
+* 이전 버전 내용: STL의 forward_list 흉내내기 (소멸자 로직 수정)
 */
 
 #pragma once
+
+// 앞으로 들이면 좋을 습관
+// 들어가고 나가는 매개변수를 통해서 들어오고 나가는 데이터를 표시할 때는 매크로를 이용하자.
+// 코드 가독성이 한결 올라간다.
+#define IN
+#define OUT
 
 #include <cstdio>
 #include <cstdlib>
@@ -21,144 +27,50 @@
 template <typename T>
 struct Node
 {
-	T data = NULL;
-	Node<T> *next = nullptr;
+	T m_data; // 노드의 데이터
+	Node<T> *m_next; // 다음 노드를 가리키기 위한 포인터
 };
 
 // C++의 STL forward_list -> 단방향 연결 리스트
 template <typename T>
-class ForwardList
+class ForwardList // C++의 단일 연결 리스트 이름을 따서 ForwardList라고 하였음.
 {
 public:
-	// 생성자 (List 생성)
-	ForwardList();
-	// 소멸자
-	~ForwardList();
-	// forward_list ADT	
-	// 노드 추가(push_front) - head로 들어가는 경우
-	void push_front(CONST T &newData);
-	// 노드 삽입(insert_after) - 특정 위치 다음에 삽입하는 경우
-	bool insert_after(Node<T>* curr, CONST T &newData);
-	// 리스트의 처음 노드 위치를 반환 (begin)
-	Node<T> *begin();
-	// 리스트의 끝 다음 노드 위치를 반환 (end)
-	Node<T> *end();
-	// 노드 삭제(pop_front) - head에서 노드 제거
-	bool pop_front(T *nodeData);
-	// 노드 삭제(erase_after) - 특정 위치 다음 노드 제거
-	bool erase_after(Node<T> *curr, T* nodeData);
-	// 노드 갯수(Count)
-	DWORD Count() const;
-private:
-	Node<T> *head; // head
-	Node<T> *tail; // tail
-	DWORD nodeCnt = 0;
-};
-
-// 생성자 (List 생성) - 더미 노드 기반
-template <typename T>
-inline ForwardList<T>::ForwardList()
-	:head(new Node<T>()), tail(new Node<T>())
-{
-	head->next = tail;
-}
-// 소멸자
-template <typename T>
-inline ForwardList<T>::~ForwardList()
-{
-	Node<T> *curr = head->next;
-	Node<T> *before = head;
-
-	// 널 포인터일 때까지
-	while (curr->next != nullptr)
+	// 생성자(Constructor)
+	ForwardList() :m_head(new Node<T>()), m_tail(new Node<T>())
 	{
-		delete before;
-		before = curr;
-		curr = curr->next;
+		m_head->m_next = m_tail;
+		m_tail->m_next = nullptr;
+	};
+	// 소멸자(Destructor)
+	~ForwardList()
+	{
+		// 더미 노드 기반이며, tail까지 있는 단일 연결 리스트.
+		// 소멸자에서는 head부터 tail까지 모두 지워버리는 케이스
+		Node<T> *curr = m_head->m_next;
+		Node<T> *before = m_head;
+
+		// 현재 가리키는 노드의 다음이 tail이 아닐 때까지 할당 해제 수행
+		while (curr->m_next != nullptr)
+		{
+			delete before;
+			before = curr;
+			curr = curr->m_next;
+		}
+		// 위의 loop가 끝나면 curr는 tail이 된다.
+		delete curr;
+	}
+	// head에 노드 삽입
+	void push_front(IN const T &in_data)
+	{
+		Node<T> *newNode = new Node<T>();
+		newNode->m_data = in_data;
+		newNode->m_next = m_head->m_next;
+		m_head->m_next = newNode;
 	}
 
-	delete curr;
-
-}
-// forward_list ADT	
-// 노드 추가(push_front) - head로 들어가는 경우
-template <typename T>
-inline void ForwardList<T>::push_front(CONST T &newData)
-{
-	Node<T> *newNode = new Node<T>();
-	newNode->data = newData;
-	newNode->next = head->next;
-	head->next = newNode;
-	nodeCnt++;
-}
-// // 노드 삽입(insert_after) - 특정 위치 다음에 삽입하는 경우
-template <typename T>
-inline bool ForwardList<T>::insert_after(Node<T> *curr, CONST T &newData)
-{
-	Node<T> *newNode = new Node<T>();
-	newNode->data = newData;
-
-	newNode->next = curr->next;
-	curr->next = newNode;
-	++nodeCnt;
-
-	return true;
-}
-
-// 리스트의 처음 노드 위치를 반환 (begin)
-template <typename T>
-inline Node<T> *ForwardList<T>::begin()
-{
-	return head->next;
-}
-
-// 리스트의 끝 다음 노드 위치를 반환 (end)
-template <typename T>
-inline Node<T> *ForwardList<T>::end()
-{
-	Node<T> *curr = head->next;
-	while (curr->next != tail)
-		curr = curr->next;
-
-	return curr;
-}
-
-// 노드 삭제(pop_front) - head에서 노드 제거
-template <typename T>
-inline bool ForwardList<T>::pop_front(T *nodeData)
-{
-	Node<T> *dNode = head->next;
-	if (dNode == tail)
-		return false;
-
-	head->next = dNode->next;
-	*nodeData = dNode->data;
-	delete dNode;
-	--nodeCnt;
-
-	return true;
-
-}
-// 노드 삭제(erase_after) - 특정 위치 다음 노드 제거
-template <typename T>
-inline bool ForwardList<T>::erase_after(Node<T> *curr, T *nodeData)
-{
-	// 노드가 없거나 하나만 있을 경우에는 특정 위치 다음에 지울 것이 없으므로 삭제하지 않는다.
-	if (curr->next == tail || head->next == tail)
-		return false;
-
-	Node<T> *dNode = curr->next;
-	curr->next = dNode->next;
-	*nodeData = dNode->data;
-	delete dNode;
-	--nodeCnt;
-
-	return true;
-}
-
-// 노드 갯수(Count)
-template <typename T>
-inline DWORD ForwardList<T>::Count() const
-{
-	return nodeCnt;
-}
+private:
+	Node<T> *m_head;
+	Node<T> *m_tail;
+	DWORD m_nodeCnt = 0;
+};
--- 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedListMain.cpp
+++ 이것이 자료구조+알고리즘이다/Ch01-List/SingleLinkedList/SingleLinkedList/SingleLinkedListMain.cpp
@@ -1,11 +1,11 @@
 ﻿/*
 * 이것이 자료구조+알고리즘이다 - 리스트(List) - 단일 연결 리스트
 * 파일명: SingleLinkedListMain.cpp
-* 파일 버전: 0.3
+* 파일 버전: 0.4
 * 작성자: Sevenshards
 * 작성 일자: 2024-02-01
-* 이전 버전 작성 일자: 2024-01-31
-* 버전 내용: STL의 forward_list 흉내내기 (프로토타입 완성)
+* 이전 버전 작성 일자: 2024-02-01
+* 버전 내용: 단일 연결 리스트 다시 구현
 * 이전 버전 내용: STL의 forward_list 흉내내기 - 클래스명 변경 및 내부 구현 변경 (미완)
 */
 
@@ -13,47 +13,6 @@
 
 int _tmain(int argc, TCHAR *argv[])
 {
-	ForwardList<INT> intList;
-
-	intList.push_front(1);
-	intList.push_front(2);
-	intList.push_front(5);
-	intList.insert_after(intList.begin(), 3);
-	intList.insert_after(intList.begin(), 4);
-
-	DWORD i = 0;
-	Node<INT> *intList_begin = intList.begin();
-	Node<INT> *intList_end = intList.end();
-
-	_tprintf(TEXT("begin: %d\n"), intList_begin->data);
-	_tprintf(TEXT("end: %d\n"), intList_end->data);
-
-	for(i = 0; i != intList.Count(); ++i)
-	{
-		_tprintf(TEXT("%d "), intList_begin -> data);
-		intList_begin = intList_begin->next;
-	}
-
-	_tprintf(TEXT("\n"));
-
-	INT nodeData;
-
-	intList.pop_front(&nodeData);
-	_tprintf(TEXT("Remove Data: %d\n"), nodeData);
-	intList.erase_after(intList.begin(), &nodeData);
-	_tprintf(TEXT("Remove Data: %d\n"), nodeData);
-
-
-	intList_begin = intList.begin();
-
-	for (i = 0; i != intList.Count(); ++i)
-	{
-		_tprintf(TEXT("%d "), intList_begin->data);
-		intList_begin = intList_begin->next;
-	}
-
-	_tprintf(TEXT("\n"));
-	_tprintf(TEXT("Node Count: %d\n"), intList.Count());
-
+	
 	return 0;
 }
