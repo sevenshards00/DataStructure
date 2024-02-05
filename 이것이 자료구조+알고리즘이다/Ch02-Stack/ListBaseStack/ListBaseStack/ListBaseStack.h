82afe8069934790f704a8a598c1959a61cb8e8ab
Sevenshards <sevenshards@hanyang.ac.kr>
2024-02-05 14:59:47 +0900

    연결 리스트 기반 스택 구현(C++, Template)

---
+++ 이것이 자료구조+알고리즘이다/Ch02-Stack/ListBaseStack/ListBaseStack/ListBaseStack.h
@@ -0,0 +1,93 @@
+﻿/*
+* 이것이 자료구조+알고리즘이다 - 스택(Stack)
+* 파일명: ListBaseStack.h
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-02-05
+* 이전 버전 작성 일자:
+* 버전 내용: 연결 리스트 기반 스택 구현(C++, Template)
+* 이전 버전 내용:
+*/
+
+#pragma once
+
+#define IN
+#define OUT
+
+#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+
+template <typename T>
+struct Node
+{
+	T m_data;
+	Node<T> *m_next = nullptr;
+};
+
+template <typename T>
+class Stack
+{
+public:
+	// 생성자
+	Stack() :m_top(new Node<T>()) {};
+	// 소멸자
+	~Stack()
+	{
+		Node<T> *curr = m_top->m_next;
+		while (curr->m_next != nullptr)
+		{
+			delete m_top;
+			m_top = curr;
+			curr = curr->m_next;
+		}
+		delete curr;
+	}
+	// isEmpty
+	bool isEmpty()
+	{
+		if (m_top->m_next == nullptr)
+			return true;
+		else
+			return false;
+	}
+	
+	// push
+	bool push(IN const T &data)
+	{
+		Node<T> *newNode = new(std::nothrow) Node<T>();
+		if (newNode == nullptr)
+			exit(-1);
+
+		newNode->m_data = data;
+		newNode->m_next = m_top->m_next;
+		m_top->m_next = newNode;
+
+		return true;
+	}
+	// pop
+	bool pop(OUT T &data)
+	{
+		if (isEmpty())
+			return false;
+		
+		Node<T> *popNode = m_top->m_next;
+		data = popNode->m_data;
+
+		m_top->m_next = popNode->m_next;
+		delete popNode;
+
+		return true;
+	}
+	// peek
+	bool peek(OUT T &data)
+	{
+		if (isEmpty())
+			return false;
+
+		data = m_top->m_next->m_data;
+		return true;
+	}
+private:
+	Node<T> *m_top;
+};
---
+++ 이것이 자료구조+알고리즘이다/Ch02-Stack/ListBaseStack/ListBaseStack/ListBaseStackMain.cpp
@@ -0,0 +1,49 @@
+﻿/*
+* 이것이 자료구조+알고리즘이다 - 스택(Stack)
+* 파일명: ListBaseStack.h
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-02-05
+* 이전 버전 작성 일자:
+* 버전 내용: 연결 리스트 기반 스택 구현(C++, Template)
+* 이전 버전 내용:
+*/
+
+#include "ListBaseStack.h"
+
+int _tmain(int argc, TCHAR *argv[])
+{
+	Stack<INT> intStack;
+	Stack<TCHAR> tcharStack;
+
+	for (int i = 0; i != 10; ++i)
+		intStack.push(i);
+
+	for (int i = 0; i != 10; ++i)
+		tcharStack.push(TEXT('A') + i);
+
+	INT idata;
+	TCHAR tdata;
+
+	for (int i = 0; i != 10; ++i)
+	{
+		intStack.peek(idata);
+		_tprintf(TEXT("peek: %d\n"), idata);
+
+		intStack.pop(idata);
+		_tprintf(TEXT("pop: %d\n"), idata);
+	}
+
+	_tprintf(TEXT("\n\n"));
+
+	for (int i = 0; i != 10; ++i)
+	{
+		tcharStack.peek(tdata);
+		_tprintf(TEXT("peek: %c\n"), tdata);
+
+		tcharStack.pop(tdata);
+		_tprintf(TEXT("pop: %c\n"), tdata);
+	}
+
+	return 0;
+}
